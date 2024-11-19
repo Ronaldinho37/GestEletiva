9007e8a352a55792ef323132150e8c47e7644be1
Alef <alefcgoncalves@outlook.com>
2024-11-19 11:25:07 -0300

    refinando

---
+++ Aplicativo/static/js/definir.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('#div-azul', {
+    delay: 200,
+    duration: 2000,
+    origin:'left',
+    distance: '250px',
+    reset: true,
+    mobile: true
+});
+
+sr.reveal('#autorization', {
+    delay: 200,
+    duration: 2000,
+    origin:'right',
+    distance: '250px',
+    reset: true,
+    mobile: true
+});
+
+sr.reveal('#habilitado', {
+    delay: 200,
+    duration: 2000,
+    origin:'top',
+    distance: '250px',
+    reset: true,
+    mobile: true
+});
